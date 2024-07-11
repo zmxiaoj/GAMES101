430a85c62d93fcd2966193f2887eaf98773a71b7
zmxj <935834551@qq.com>
2024-07-11 19:51:16 +0800

    Assignment3: Finished hw3

--- Assignment3/Code/Texture.hpp
+++ Assignment3/Code/Texture.hpp
@@ -24,9 +24,38 @@ public:
 
     Eigen::Vector3f getColor(float u, float v)
     {
+        if (u < 0) u = 0.0f;
+        if (u > 1) u = 1.0f;
+        if (v < 0) v = 0.0f;
+        if (v > 1) v = 1.0f;
         auto u_img = u * width;
         auto v_img = (1 - v) * height;
         auto color = image_data.at<cv::Vec3b>(v_img, u_img);
+        return Eigen::Vector3f(color[0], color[1], color[2]);
+    }
+    // bilinear interpolation 效果不佳
+    Eigen::Vector3f getColorBilinear(float u, float v)
+    {
+ 
+        if(u<0) u=0;
+        if(v<0) v=0;
+        if(u>1) u=1;
+        if(v>1) v=1;
+   
+        auto u_img = u * (width);
+        auto v_img = (1 - v) * (height);
+        float u0 = std::max(1.0,floor(u_img-0.5)), u1 = floor(u_img+0.5);
+        float v0 = std::max(1.0,floor(v_img-0.5)), v1 = floor(v_img+0.5);
+        float s = (u_img-u0)/(u1-u0);
+        float t = (v_img-v0)/(v1-v0);
+   
+        auto color00 = image_data.at<cv::Vec3b>(v0, u0);
+        auto color01 = image_data.at<cv::Vec3b>(v0, u1);
+        auto color10 = image_data.at<cv::Vec3b>(v1, u0);
+        auto color11 = image_data.at<cv::Vec3b>(v1, u1);
+        auto color0 = color00 + s*(color01-color00);
+        auto color1 = color10 + s*(color11-color10);
+        auto color = color0 + t*(color1-color0);
         return Eigen::Vector3f(color[0], color[1], color[2]);
     }
 
